7922246f5817ba2bcd5e6a8c8abebc06b107cd2f
Fangspectr <83934976+Fangspectr@users.noreply.github.com>
2024-02-11 17:50:55 -0500

    Add files via upload

--- functions.js
+++ functions.js
@@ -3,9 +3,9 @@ document.getElementById('modeToggle').addEventListener('click', function() {
     var body = document.body;
     if (body.classList.contains('light-mode')) {
         body.classList.replace('light-mode', 'dark-mode');
-        this.textContent = 'Switch to Light Mode';
+        this.textContent = '💡';
     } else {
         body.classList.replace('dark-mode', 'light-mode');
-        this.textContent = 'Switch to Dark Mode';
+        this.textContent = '🌙';
     }
 });
